f2ea8f151c5b3984b018d67b29d92290d8b0dd95
LoganFan <fanliugen@gmail.com>
2020-10-02 17:38:25 +0800

    添加首页展示效果

--- gallery/models.py
+++ gallery/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
-    # class Meta:
-    #     verbose_name = "Gallery"
-    #     verbose_name_plural = "Gallerys"
+    description = models.CharField(default='在这里写作品的简介',max_length=100)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='作品标题', max_length= 50)
 
-    # def __str__(self):
-    #     pass
+
+    def __str__(self):
+        return self.title
