b398fba7898a407c1c073b37fe5558c720527b67
LoganFan <fanliugen@gmail.com>
2020-10-02 19:12:49 +0800

    美化blog页面

--- blog/urls.py
+++ blog/urls.py
@@ -13,17 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path ,include
-from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
+from django.urls import path
+
+from blog.views import blog_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home),
-    path('blog/', include('blog.urls')),
 
+    path('',blog_page)
 
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
